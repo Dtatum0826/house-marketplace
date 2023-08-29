39bf73b1032295da753c433ae468a8b3ac874c83
Dustin Tatum <dtatum0826@gmail.com>
2023-08-29 18:58:32 -0500

    Sign in and sign up components with links added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Explore from "./pages/Explore";
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
-import SignIn from "./pages/SignIn";
+import SignIn from "./pages/SignIn"; 
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import NavBar from "./components/NavBar";
