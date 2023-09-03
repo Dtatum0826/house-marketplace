50f9f1d3be86ecd1862e837593d9a3c7b8bd1ecd
Dustin Tatum <dtatum0826@gmail.com>
2023-09-03 16:29:12 -0500

    added Private route for profile

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn"; 
 import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import NavBar from "./components/NavBar";
 
@@ -16,7 +17,9 @@ function App() {
       <Routes>
         <Route path='/' element= {<Explore/>}/>
         <Route path='/offers' element= {<Offers/>}/>
-        <Route path='/profile' element= {<Profile/>}/>
+        <Route path='/profile' element={<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
         <Route path='/sign-in' element= {<SignIn/>}/>
         <Route path='/sign-up' element= {<SignUp/>}/>
         <Route path='/forgot-password' element= {<ForgotPassword/>}/>
