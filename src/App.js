65fdcd755a3dfc19c2dc0371c208174b55cb81de
Dustin Tatum <dtatum0826@gmail.com>
2023-09-03 19:45:54 -0500

    fixed pathnames for category component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn"; 
 import SignUp from "./pages/SignUp";
 import Category from "./pages/Category";
-import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
+import PrivateRoute from "./components/PrivateRoute";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element= {<Explore/>}/>
         <Route path='/offers' element= {<Offers/>}/>
-        <Route path='/category:categoryName' element= {<Category/>}/>
+        <Route path='/category/:categoryName' element= {<Category/>}/>
         <Route path='/profile' element={<PrivateRoute/>}>
           <Route path="/profile" element={<Profile/>}/>
         </Route>
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -15,14 +15,14 @@ function Explore() {
 
       <p className="exploreCategoryHeading">Categories</p>
       <div className="exploreCategories">
-        <Link to='/categories/rent'>
+        <Link to='/category/rent'>
           <img src={rentCategoryImage}
           alt='rent'
           className='exploreCategoryImg'
           />
           <p className="exploreCategoryName">Places for Rent</p>
         </Link>
-        <Link to='/categories/sell'>
+        <Link to='/category/sale'>
           <img src={sellCategoryImage}
           alt='sell'
           className='exploreCategoryImg'
