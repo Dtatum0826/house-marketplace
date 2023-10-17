c8f5c08757f7c7b0c7ef13e390b1e287536e68bf
Dustin Tatum <dtatum0826@gmail.com>
2023-10-17 18:13:52 -0500

    Added pagination to offers and categories pages

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -18,6 +18,7 @@ import LisitngItem from '../components/LisitngItem'
 function Category() {
  const [listings, setListings] = useState(null)
  const[loading, setLoading] = useState(true)
+ const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
  const params = useParams()
 
@@ -33,11 +34,12 @@ function Category() {
         listingsRef,
          where('type','==', params.categoryName),
          orderBy('timestamp', 'desc'),
-         limit(10)
+         limit(5)
          )
          
         const querySnap = await getDocs(q)
-       
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+        setLastFetchedListing(lastVisible)
        
         
        let listings =[]
@@ -55,6 +57,41 @@ function Category() {
   }
   fecthListings()
  },[params.categoryName])
+
+
+  const onFecthMoreListings = async () =>{
+    try {
+      const listingsRef = collection(db,'listings')
+     
+    
+      
+      const q = query(
+        listingsRef,
+         where('type','==', params.categoryName),
+         orderBy('timestamp', 'desc'),
+         startAfter(lastFetchedListing),
+         limit(5)
+         )
+         
+        const querySnap = await getDocs(q)
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+        setLastFetchedListing(lastVisible)
+       
+        
+       let listings =[]
+        querySnap.forEach((doc)=>{
+          return listings.push({
+            id: doc.id,
+            data: doc.data()
+          })
+        })
+        setListings((prevState)=>[...prevState,...listings])
+        setLoading(false)
+    } catch (error) {
+      toast.error('Could not fetch listings')
+    }
+  }
+ 
 
   return (
     <div className="category">
@@ -78,6 +115,12 @@ function Category() {
           ))}
         </ul>
       </main>
+
+      <br/>
+      <br/>
+      {lastFetchedListing && (
+        <p className="loadMore" onClick={onFecthMoreListings}>Load More</p>
+      )}
       </> : <p>No Listings for {params.categoryName}</p>}
     </div>
   )
